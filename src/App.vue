4d9498ef5b98d010e0b029fbbd9f27d8b164329f
AndMJ <andrejoao070@gmail.com>
2023-02-07 02:55:08 +0000

    ADD ability to search cars by name
    ADD ability to add cars to the local list

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
-    <HeaderComponent></HeaderComponent>
+    <HeaderComponent @car-to-search="getNameToSearch"></HeaderComponent>
     <div class="container">
-      <router-view></router-view>
+      <router-view :cars="cars" @add-new-car="addCar" :CarName="carToSearch"></router-view>
     </div>
   </main>
 </template>
@@ -13,6 +13,31 @@ export default {
   name: "App",
   components: {
     HeaderComponent
+  },
+  data(){
+    return {
+      cars: [],
+      carToSearch: ""
+    }
+  },
+  methods: {
+    getNameToSearch(carName){
+      this.carToSearch = carName
+    },
+
+    //ADD NEW CAR
+    addCar(car){
+      let new_car = {
+        id: (Math.random() * 1000),
+        name: car.name,
+        year: car.year
+      }
+
+      this.cars.push(new_car)
+    }
+  },
+  beforeMount() {
+    this.cars.push({ id:4562, name:"Toyota", year:"1999" },{ id:7892, name:"Nissan", year:"2000" },{ id:8729, name:"Mazda", year:"1998" })
   }
 }
 </script>

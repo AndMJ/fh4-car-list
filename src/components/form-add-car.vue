4e0bdab30994b6e33a9bcfa9ccd91f7aa1a43250
AndMJ <andrejoao070@gmail.com>
2023-02-07 02:56:56 +0000

    ADD addNewCar() & completed form

--- src/components/form-add-car.vue
+++ src/components/form-add-car.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="row">
-    <form>
+    <form @submit="AddNewCar">
       <div class="col-6 mb-3">
         <label for="name" class="form-label">Car Name</label>
-        <input class="form-control" name="name" placeholder="car name">
+        <input class="form-control" v-model="name" name="name" placeholder="car name">
       </div>
       <div class="col-6 mb-3">
         <label for="year" class="form-label">Car Year</label>
-        <input class="form-control" name="year" placeholder="car year">
+        <input class="form-control" v-model="year" name="year" placeholder="car year">
       </div>
       <button type="submit" class="btn btn-success">save</button>
     </form>
@@ -16,10 +16,20 @@
 
 <script>
 export default {
-  name: "form-add-car"
+  name: "form-add-car",
+  data(){
+    return {
+      name: "",
+      year: ""
+    }
+  },
+  methods:{
+    AddNewCar(e){
+      e.preventDefault()
+
+      this.$emit('add-new-car', {name: this.name, year: this.year})
+    }
+  },
+  emits: ['add-new-car']
 }
 </script>
-
-<style scoped>
-
-</style>

876f69aa257da34024576039eddabe43c0857d82
AndMJ <andrejoao070@gmail.com>
2023-03-28 18:04:59 +0100

    house cleaning

--- src/components/card-component.vue
+++ src/components/card-component.vue
@@ -2,13 +2,14 @@
   <div class="card">
     <img :src="[carData.image ? require('@/assets/car_images/' + carData.image) : require('@/assets/flat-retro-car.svg')]" class="card-img-top" alt="carData.name">
     <div class="card-body">
+      <p hidden>{{carData.id}}</p>
       <h3 class="card-title">{{ carData.manufacturer }} &sdot; {{ carData.name }}</h3>
       <h5 class="card-text">{{ carData.year }} &sdot; {{ carData.type }}</h5>
 
     </div>
     <div class="card-footer">
       <div class="row">
-        <p class="col card-text">ID: {{ carData.id }}</p>
+        <p class="col card-text">ID: {{ carData.inGameID }}</p>
       </div>
     </div>
   </div>

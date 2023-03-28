7ae71da0818a24332e373b45dbe1ba7e309b38fd
AndMJ <andrejoao070@gmail.com>
2023-03-28 18:05:14 +0100

    ADD carModel.js

---
+++ src/models/carModel.js
@@ -0,0 +1,28 @@
+
+async function getCarList (url) {
+    let res
+    await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then((response) => response.json())
+    .then(resBody => {
+        res = resBody
+    })
+
+    return res
+}
+
+async function getCarById (url, id) {
+
+    await fetch(url+"/"+id, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+export default {getCarList,getCarById}

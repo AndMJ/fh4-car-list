292c277b0d1d80724489767a32777c4f7cbc4b03
AndMJ <andrejoao070@gmail.com>
2023-03-28 18:05:35 +0100

    ADD api url global variable

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+let app = createApp(App)
+
+// GLOBALS DECLARATION
+// API_URL
+app.config.globalProperties.$API_URL = "http://127.0.0.1:8080"
+
+// ROUTER
+app.use(router).mount('#app')
+

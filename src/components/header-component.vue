dd1af68162227e98630c84ffc53a5ba5051bef4e
AndMJ <andrejoao070@gmail.com>
2023-04-21 02:12:43 +0100

    ADD header route link to /users view

--- src/components/header-component.vue
+++ src/components/header-component.vue
@@ -9,6 +9,7 @@
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><router-link to="/" :class="this.$route.path=='/' ? 'nav-link text-white' : 'nav-link text-secondary'">Home</router-link></li>
           <li><router-link to="/about" :class="this.$route.path=='/about' ? 'nav-link text-white' : 'nav-link text-secondary'">About</router-link></li>
+          <li><router-link to="/users" :class="this.$route.path=='/users' ? 'nav-link text-white' : 'nav-link text-secondary'">Users</router-link></li>
         </ul>
 
         <form @submit.prevent class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">

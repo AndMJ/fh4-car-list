8ac2a382b179a7eb4ba0705dbc46d8cb8b6e427f
AndMJ <andrejoao070@gmail.com>
2023-04-21 02:10:56 +0100

    ADD user data component

---
+++ src/components/user-component.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul class="list-group">
+        <li class="list-group-item">{{ user.name }}</li>
+        <li class="list-group-item">{{ user.email }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "user-component",
+    props: {
+        user: Array
+    }
+}
+</script>
